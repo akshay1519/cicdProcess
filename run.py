1fc5becfc6e2935f54baa241589b8614deb540ca
Akshay G <akshay_h_g@hotmail.com>
2024-05-05 08:58:38 +0530

    modify the conditional logic for first time run

--- run.py
+++ run.py
@@ -18,24 +18,27 @@ print(latest_commit_sha)
 
 repo_url = repo.clone_url
 destination_dir = "repo"
+
 if os.path.exists("commit_hash.txt"):
-    commit_hash_file = open("commit_hash.txt", "r")
-    previous_commit_sha = commit_hash_file.read()
-    commit_hash_file.close()
-    print(previous_commit_sha)
+    with open("commit_hash.txt", "r") as commit_hash_file:
+        previous_commit_sha = commit_hash_file.read()
+
     if previous_commit_sha == latest_commit_sha:
         print("No new commits")
         exit(0)
-    else:
-        with open("commit_hash.txt", "w") as file:
-            file.write(latest_commit_sha)
-        if os.path.exists(destination_dir):
-            shutil.rmtree(destination_dir)
-        subprocess.run(["git", "clone", repo_url, destination_dir])
-        print("Cloned repo")
-        repo_url = repo.clone_url
-        destination_dir = "repo"
-        subprocess.run(["bash", "deployment.sh"])
+else:
+    print("Running for the first time")
+
+with open("commit_hash.txt", "w") as file:
+    file.write(latest_commit_sha)
+
+if os.path.exists(destination_dir):
+    shutil.rmtree(destination_dir)
+
+subprocess.run(["git", "clone", repo_url, destination_dir])
+print("Cloned repo")
+
+subprocess.run(["bash", "deployment.sh"])
 
 commit_hash_file = open("commit_hash.txt", "w")
 commit_hash_file.write(latest_commit_sha)
